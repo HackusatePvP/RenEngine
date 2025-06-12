76114e70ac37226df4bdf4de3346d2bba9a4ba71
piitex <supermftw@gmail.com>
2025-06-12 08:46:34 -0500

    Made improvements and fixed issues with ScrollContainer.

--- src/main/java/me/piitex/engine/containers/ScrollContainer.java
+++ src/main/java/me/piitex/engine/containers/ScrollContainer.java
@@ -14,6 +14,9 @@ public class ScrollContainer extends Container {
     private final Layout layout;
     private ScrollPane scrollPane;
     private double xOffset, yOffset;
+    private boolean horizontalScroll = true;
+    private boolean verticalScroll = true;
+    private boolean scrollWhenNeeded = true;
 
     public ScrollContainer(Layout layout, double x, double y, double width, double height) {
         super(x, y, width, height);
@@ -36,20 +39,61 @@ public class ScrollContainer extends Container {
         this.yOffset = yOffset;
     }
 
+    public boolean isHorizontalScroll() {
+        return horizontalScroll;
+    }
+
+    public void setHorizontalScroll(boolean horizontalScroll) {
+        this.horizontalScroll = horizontalScroll;
+    }
+
+    public boolean isVerticalScroll() {
+        return verticalScroll;
+    }
+
+    public void setVerticalScroll(boolean verticalScroll) {
+        this.verticalScroll = verticalScroll;
+    }
+
+    public boolean isScrollWhenNeeded() {
+        return scrollWhenNeeded;
+    }
+
+    public void setScrollWhenNeeded(boolean scrollWhenNeeded) {
+        this.scrollWhenNeeded = scrollWhenNeeded;
+    }
+
+    public ScrollPane getScrollPane() {
+        return scrollPane;
+    }
+
     @Override
     public Map.Entry<Node, LinkedList<Node>> build() {
         scrollPane = new ScrollPane();
         scrollPane.setTranslateX(getX());
         scrollPane.setTranslateY(getY());
         scrollPane.setPrefSize(getWidth(), getHeight());
-        scrollPane.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.ALWAYS);
+        if (verticalScroll) {
+            scrollPane.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.ALWAYS);
+        } else if (scrollWhenNeeded) {
+            scrollPane.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        } else {
+            scrollPane.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+        }
+        if (horizontalScroll) {
+            scrollPane.hbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.ALWAYS);
+        } else if (scrollWhenNeeded) {
+            scrollPane.hbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        } else {
+            scrollPane.hbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+        }
 
         // Build pane layout for the scroll content
-        Pane pane = layout.getPane();
+        Pane pane = layout.render(this);
         scrollPane.setContent(pane);
 
         LinkedList<Node> order = buildBase();
-        order.add(layout.render(this));
+        order.add(pane);
 
         // Offset overlays by 10
         if (xOffset > 0 || yOffset > 0) {
