572f2b4456b96cc3b450dbeee3122b0d264fad90
piitex <Supermftw@gmail.com>
2025-12-12 08:07:50 -0600

    Added RingProgressOverlay.

---
+++ src/main/java/me/piitex/engine/overlays/RingProgressOverlay.java
@@ -0,0 +1,117 @@
+package me.piitex.engine.overlays;
+
+import atlantafx.base.controls.RingProgressIndicator;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Node;
+
+public class RingProgressOverlay extends Overlay implements Region {
+    private double width, height, prefWidth, prefHeight, maxWidth, maxHeight;
+    private double scaleWidth, scaleHeight;
+    private final RingProgressIndicator progressBar;
+
+    public RingProgressOverlay() {
+        this.progressBar = new RingProgressIndicator();
+        setNode(progressBar);
+    }
+
+    public void bind(ObservableValue<? extends Number> binding) {
+        progressBar.progressProperty().bind(binding);
+    }
+
+    @Override
+    public Node render() {
+        progressBar.setTranslateX(getX());
+        progressBar.setTranslateY(getY());
+
+        if (getWidth() > 0 || getHeight() > 0) {
+            progressBar.setMinSize(width, height);
+        }
+        if (getPrefWidth() > 0 || getPrefHeight() > 0) {
+            progressBar.setPrefSize(getPrefWidth(), getPrefHeight());
+        }
+        if (getMaxWidth() > 0 || getMaxHeight() > 0) {
+            progressBar.setMaxSize(getMaxWidth(), getMaxHeight());
+        }
+
+        progressBar.getStyleClass().addAll(getStyles());
+
+        return progressBar;
+    }
+
+    public RingProgressIndicator getProgressBar() {
+        return progressBar;
+    }
+
+    @Override
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public void setWidth(double w) {
+        this.width = w;
+        progressBar.setMinWidth(w);
+    }
+
+    @Override
+    public void setHeight(double h) {
+        this.height = h;
+        progressBar.setMinHeight(h);
+    }
+
+    @Override
+    public double getPrefWidth() {
+        return prefWidth;
+    }
+
+    @Override
+    public double getPrefHeight() {
+        return prefHeight;
+    }
+
+    @Override
+    public void setPrefWidth(double w) {
+        this.prefWidth = w;
+        progressBar.setPrefWidth(w);
+    }
+
+    @Override
+    public void setPrefHeight(double h) {
+        this.prefHeight = h;
+        progressBar.setPrefHeight(h);
+    }
+
+    @Override
+    public double getMaxWidth() {
+        return maxWidth;
+    }
+
+    @Override
+    public double getMaxHeight() {
+        return maxHeight;
+    }
+
+    @Override
+    public void setMaxWidth(double w) {
+        this.maxWidth = w;
+        progressBar.setMaxWidth(w);
+    }
+
+    @Override
+    public void setMaxHeight(double h) {
+        this.maxHeight = h;
+        progressBar.setMaxHeight(h);
+    }
+
+    @Override
+    public void setMaxSize(double w, double h) {
+        this.maxWidth = w;
+        this.maxHeight = h;
+        progressBar.setMaxSize(w, h);
+    }
+}
