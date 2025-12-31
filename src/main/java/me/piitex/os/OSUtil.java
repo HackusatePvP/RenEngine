5a5a78f8474ac8c786607e77bc452db5ed29dd81
HackusatePvP <supermftw@gmail.com>
2025-12-30 18:59:24 -0600

    Added oshi-core.

--- src/main/java/me/piitex/os/OSUtil.java
+++ src/main/java/me/piitex/os/OSUtil.java
@@ -1,17 +1,16 @@
 package me.piitex.os;
 
-import org.apache.commons.lang3.SystemUtils;
+import oshi.SystemInfo;
+import oshi.software.os.OperatingSystem;
 
 /**
  * Simple utility for gathering operating system information.
  */
 public class OSUtil {
+    private static final SystemInfo SYSTEM_INFO = new SystemInfo();
+    private static final OperatingSystem OS = SYSTEM_INFO.getOperatingSystem();
 
     public static String getOS() {
-        return SystemUtils.OS_NAME;
-    }
-
-    public static String getVersion() {
-        return SystemUtils.OS_VERSION;
+        return OS.toString(); // Returns something like "Ubuntu 22.04.1 LTS"
     }
 }
