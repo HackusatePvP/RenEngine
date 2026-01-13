e289e502ef445664d79f6d7e1f4b8a3e4b41431b
HackusatePvP <supermftw@gmail.com>
2026-01-13 14:06:10 -0600

    Added graphics card information.

--- src/main/java/me/piitex/os/OSUtil.java
+++ src/main/java/me/piitex/os/OSUtil.java
@@ -1,7 +1,10 @@
 package me.piitex.os;
 
 import oshi.SystemInfo;
+import oshi.hardware.GraphicsCard;
 import oshi.software.os.OperatingSystem;
+
+import java.util.List;
 
 /**
  * Simple utility for gathering operating system information.
@@ -12,5 +15,13 @@ public class OSUtil {
 
     public static String getOS() {
         return OS.toString(); // Returns something like "Ubuntu 22.04.1 LTS"
+    }
+
+    public List<GraphicsCard> getGraphicsCards() {
+        return SYSTEM_INFO.getHardware().getGraphicsCards();
+    }
+
+    public long getGraphicsTotalVRAM(GraphicsCard graphicsCard) {
+        return graphicsCard.getVRam();
     }
 }
